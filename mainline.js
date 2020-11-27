26b3794e818082c4ce822d0096a5f5ff9e62df28
tonetheman <tonetheman@gmail.com>
2020-11-27 10:44:25 -0600

    muting added

--- mainline.js
+++ mainline.js
@@ -30,7 +30,25 @@ function paint_background() {
     document.body.style.backgroundImage = `url('${imgs[choice]}')`;
 }
 
+function handle_muting() {
+    function resFunc(a) {
+        //console.log(a);
+    }
+
+   // get the current tab that was just opened
+    chrome.tabs.getCurrent((t) => {
+        //console.log(t);
+	// update the tab to be muted
+	// the only reason I added the callback func resFunc
+	// is for debugging
+        chrome.tabs.update(t.id,
+            {muted: true}, resFunc);
+    });
+};
+
 function main() {
+    handle_muting();
+
     paint_background();
     paint();
     setInterval(paint, 1000);
--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
 
 # simple replacment for newtab for chrome
 
+2020-11-27: added the muting of all tabs
+- had to add tabs into manifest json for this reason
+
+2020-11-22: started this project
+
 Uses some photos from unsplash and a simple time display.
 
 Get a picture from unsplash.
